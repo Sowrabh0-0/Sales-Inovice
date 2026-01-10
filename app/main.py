66a2f8de4388c3e185085e52267b6494651fe2dc
sowrabh0-0 <sksseervi@gmail.com>
2026-01-10 11:59:29 +0530

    Added Routing to main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import engine, Base
-# from app.routers import orders, invoices, payments   # your separated folders already exist
+from app.routers import orders, invoices, payments
 
 
 app = FastAPI(
@@ -14,14 +14,12 @@ def health():
     return {"status": "running"}
 
 
-# # -----------------------------
-# # REGISTER ROUTERS
-# # -----------------------------
-# app.include_router(orders.router)
-# app.include_router(invoices.router)
-# app.include_router(payments.router)
+
+app.include_router(orders.router)
+app.include_router(invoices.router)
+app.include_router(payments.router)
 
 
-# optional: allow script check
+
 if __name__ == "__main__":
     print("Use → uvicorn app.main:app --reload")
